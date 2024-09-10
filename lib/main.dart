3c8a83b75a133c36fbbee6d0cef8330ecf6ba834
abdumann0n <abdumannonabduxannonov@gmail.com>
2024-09-10 18:49:26 +0500

    2 commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class myapp extends StatelessWidget {
+  const myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
